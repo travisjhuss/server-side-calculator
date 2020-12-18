d38cf8fc98fbee03a109b17fe844aebf160c60ad
Travis Huss <travisjhuss@gmail.com>
2020-12-18 17:54:25 -0600

    connected bootstrap. set up click functions for all buttons

--- server/public/client.js
+++ server/public/client.js
@@ -2,5 +2,42 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery loaded.');
-    
+
+    $('#equalBtn').on('click', runCalculator);
+    $('#plusBtn').on('click', plusOperator);
+    $('#minusBtn').on('click', minusOperator);
+    $('#multiplyBtn').on('click', multiplyOperator);
+    $('#divideBtn').on('click', divideOperator);
+    $('#clearBtn').on('click', clearCalc);
+
 } // end onReady
+
+function runCalculator() {
+    console.log('clicked equals');
+
+} // end runCalculator
+
+function plusOperator() {
+    console.log('clicked plus');
+
+} // end plusOperator
+
+function minusOperator() {
+    console.log('clicked minus');
+
+} // end minusOperator
+
+function multiplyOperator() {
+    console.log('clicked multiply');
+
+} // end multiplyOperator
+
+function divideOperator() {
+    console.log('clicked divide');
+
+} // end divideOperator
+
+function clearCalc() {
+    console.log('clicked clear');
+
+} // end clearCalc
--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-
+// array for calculation history
+let calculationHistory = [];
 
 
 
