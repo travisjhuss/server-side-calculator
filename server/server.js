14d9a72524f7212a30fcfac32ead5c00d670e425
Travis Huss <travisjhuss@gmail.com>
2020-12-18 19:09:41 -0600

    start calculateObject function. is recieving posted object. recognizing selected math operator

--- server/server.js
+++ server/server.js
@@ -3,11 +3,31 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 // array for calculation history
 let calculationHistory = ['test', 'one', 'two'];
+
+function calculateObject(object) {
+    console.log('object to run:', object);
+    switch (object.operator) {
+        case "+":
+            console.log(`${object.numberOne} plus ${object.numberTwo}`);
+            break;
+        case "-":
+            console.log(`${object.numberOne} minus ${object.numberTwo}`);
+            break;
+        case "*":
+            console.log(`${object.numberOne} times ${object.numberTwo}`);
+            break;
+        case "/":
+            console.log(`${object.numberOne} divided by ${object.numberTwo}`);
+            break;
+
+    } // end switch
+
+} // end calculateObject
 
 
 // GET route
@@ -17,8 +37,10 @@ app.get('/calculator', (req, res) => {
 });
 
 app.post('/calculator', (req, res) => {
-    let postObject = req.body;
-    console.log(postObject);
+    let numbersToCrunch = req.body;
+    // console.log(numbersToCrunch);
+    // call application to run math
+    calculateObject(numbersToCrunch);
     res.sendStatus(201);
 });
 
