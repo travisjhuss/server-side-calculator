c01d04068f2573ae75cb38a1f0a776ff438bc823
Travis Huss <travisjhuss@gmail.com>
2020-12-19 12:42:24 -0600

    added functions each button to append to calc display screen

--- server/public/client.js
+++ server/public/client.js
@@ -9,6 +9,16 @@ function onReady() {
     $('#multiplyBtn').on('click', multiplyOperator);
     $('#divideBtn').on('click', divideOperator);
     $('#clearBtn').on('click', clearCalc);
+    $('#zeroBtn').on('click', zeroButton);
+    $('#oneBtn').on('click', oneButton);
+    $('#twoBtn').on('click', twoButton);
+    $('#threeBtn').on('click', threeButton);
+    $('#fourBtn').on('click', fourButton);
+    $('#fiveBtn').on('click', fiveButton);
+    $('#sixBtn').on('click', sixButton);
+    $('#sevenBtn').on('click', sevenButton);
+    $('#eightBtn').on('click', eightButton);
+    $('#nineBtn').on('click', nineButton);
 
 } // end onReady
 
@@ -34,6 +44,66 @@ function runCalculator() {
     });
 
 } // end runCalculator
+
+function zeroButton() {
+    console.log('clicked zero');
+    
+} // end zeroButton
+
+function oneButton () {
+    console.log('clicked one');
+    $('#calcScreen').append('1');
+
+} // end oneButton
+
+function twoButton () {
+    console.log('clicked two');
+    $('#calcScreen').append('2');
+
+} // end twoButton
+
+function threeButton () {
+    console.log('clicked three');
+    $('#calcScreen').append('3');
+
+} // end threeButton
+
+function fourButton () {
+    console.log('clicked four');
+    $('#calcScreen').append('4');
+
+} // end fourButton
+
+function fiveButton () {
+    console.log('clicked five');
+    $('#calcScreen').append('5');
+
+} // end fiveButton
+
+function sixButton () {
+    console.log('clicked six');
+    $('#calcScreen').append('6');
+
+} // end sixButton
+
+function sevenButton () {
+    console.log('clicked seven');
+    $('#calcScreen').append('7');
+
+} // end sevenButton
+
+function eightButton () {
+    console.log('clicked eight');
+    $('#calcScreen').append('8');
+
+} // end eightButton
+
+function nineButton () {
+    console.log('clicked nine');
+    $('#calcScreen').append('9');
+
+} // end nineButton
+
 
 function plusOperator() {
     // console.log('clicked plus');
@@ -65,6 +135,8 @@ function clearCalc() {
     $('#secondNumIn').val('');
     delete equationObject.operator;
 } // end clearCalc
+
+// ------------ end button functions ------------ //
 
 // GET function
 function getResponseFromServer() {
